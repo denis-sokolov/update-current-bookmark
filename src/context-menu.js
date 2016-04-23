4e1d9c6edc35b0d4a956dc2330d9899dab1096b0
Denis Sokolov <denis@sokolov.cc>
2016-04-23 11:05:58 +0200

    Update the bookmark

--- src/context-menu.js
+++ src/context-menu.js
@@ -5,6 +5,26 @@ chrome.contextMenus.create({
   contexts: ['link']
 });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab){
-  console.log(info);
+var say = function(id){
+  alert(chrome.i18n.getMessage(id));
+}
+
+chrome.contextMenus.onClicked.addListener(function(info){
+  chrome.permissions.request({
+    permissions: ['bookmarks']
+  }, function(granted) {
+    if (!granted) return;
+    chrome.bookmarks.search({
+      url: info.pageUrl
+    }, function(results){
+      if (results.length === 0)
+        return say('current_page_not_bookmarked');
+      if (results.length > 1)
+        return say('current_page_bookmarked_multiple_times');
+      var current = results[0];
+      chrome.bookmarks.update(current.id, {
+        url: info.linkUrl
+      });
+    });
+  });
 });
