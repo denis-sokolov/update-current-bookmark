dc34688f6e5f5e0ad8a2f27d9bbecf062285d38c
Denis Sokolov <denis@sokolov.cc>
2016-05-01 02:15:49 +0200

    Refactor findBookmarkId out

--- src/context-menu.js
+++ src/context-menu.js
@@ -9,22 +9,27 @@ var say = function(id){
   alert(chrome.i18n.getMessage(id));
 }
 
+var findBookmarkId = function(currentUrl, callback){
+  chrome.bookmarks.search({
+    url: currentUrl
+  }, function(results){
+    if (results.length === 0)
+      return say('current_page_not_bookmarked');
+    if (results.length > 1)
+      return say('current_page_bookmarked_multiple_times');
+    callback(results[0].id);
+  });
+}
+
 chrome.contextMenus.onClicked.addListener(function(info){
   chrome.permissions.request({
     permissions: ['bookmarks']
   }, function(granted) {
     if (!granted) return;
-    chrome.bookmarks.search({
-      url: info.pageUrl
-    }, function(results){
-      if (results.length === 0)
-        return say('current_page_not_bookmarked');
-      if (results.length > 1)
-        return say('current_page_bookmarked_multiple_times');
-      var current = results[0];
-      chrome.bookmarks.update(current.id, {
+    findBookmarkId(info.pageUrl, function(id){
+      chrome.bookmarks.update(id, {
         url: info.linkUrl
       });
-    });
+    })
   });
 });
