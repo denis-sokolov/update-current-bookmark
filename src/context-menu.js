c9985e4c7d14b1299ac00bed8757b6d153531181
Denis Sokolov <denis@sokolov.cc>
2024-04-19 12:00:00 +0000

    Format code

--- src/context-menu.js
+++ src/context-menu.js
@@ -1,35 +1,39 @@
-
 chrome.contextMenus.create({
-  id: 'update-bookmark',
-  title: chrome.i18n.getMessage('update_current_bookmark'),
-  contexts: ['link']
+  id: "update-bookmark",
+  title: chrome.i18n.getMessage("update_current_bookmark"),
+  contexts: ["link"],
 });
 
-var say = function(id){
+var say = function (id) {
   alert(chrome.i18n.getMessage(id));
-}
+};
 
-var findBookmarkId = function(currentUrl, callback){
-  chrome.bookmarks.search({
-    url: currentUrl
-  }, function(results){
-    if (results.length === 0)
-      return say('current_page_not_bookmarked');
-    if (results.length > 1)
-      return say('current_page_bookmarked_multiple_times');
-    callback(results[0].id);
-  });
-}
+var findBookmarkId = function (currentUrl, callback) {
+  chrome.bookmarks.search(
+    {
+      url: currentUrl,
+    },
+    function (results) {
+      if (results.length === 0) return say("current_page_not_bookmarked");
+      if (results.length > 1)
+        return say("current_page_bookmarked_multiple_times");
+      callback(results[0].id);
+    },
+  );
+};
 
-chrome.contextMenus.onClicked.addListener(function(info){
-  chrome.permissions.request({
-    permissions: ['bookmarks']
-  }, function(granted) {
-    if (!granted) return;
-    findBookmarkId(info.pageUrl, function(id){
-      chrome.bookmarks.update(id, {
-        url: info.linkUrl
+chrome.contextMenus.onClicked.addListener(function (info) {
+  chrome.permissions.request(
+    {
+      permissions: ["bookmarks"],
+    },
+    function (granted) {
+      if (!granted) return;
+      findBookmarkId(info.pageUrl, function (id) {
+        chrome.bookmarks.update(id, {
+          url: info.linkUrl,
+        });
       });
-    })
-  });
+    },
+  );
 });
